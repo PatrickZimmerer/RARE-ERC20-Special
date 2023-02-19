747192733597b539e643d7351cbfb6f50dff7998
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-02-19 10:24:02 +0100

    added basic capped coin and sanctions coin

--- contracts/ERC20Sanctions.sol
+++ contracts/ERC20Sanctions.sol
@@ -1,2 +1,79 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title A contract for a basic ERC20 coin where specific users can be banned from using it
+ * @author Patrick Zimmerer
+ * @notice This contract is to demo a sample ERC20 token with sanctions
+ * @dev When deploying you can choose a token name & symbol => deployer == owner
+ * @dev the owner also is the admin who can ban and unban addresses
+ */
+contract ERC20Sanctions is ERC20Capped, Ownable {
+    uint256 public constant MAX_SUPPLY = 100_000_000 * 1e18;
+    address immutable i_owner;
+
+    mapping(address => uint256) bannedUsers; // using uint instead of bool to reduce gas cost
+
+    modifier onlyUnbanned() {
+        require(bannedUsers[msg.sender] != 1, "You are banned");
+        _;
+    }
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) ERC20Capped(MAX_SUPPLY) {
+        i_owner = payable(msg.sender);
+        _mint(msg.sender, 1_000_000 * 10 ** uint256(decimals()));
+    }
+
+    /**
+     * @notice Only admin can ban/unban users from using the contract
+     * @dev If you want to ban a User pass in the number 1 if you want to unban the user
+     * @dev it is recommended to pass in a number > 1 like 2 since setting
+     * @dev a non-zero to a non-zero value costs only 5000 gas instead of 20_000gas
+     */
+    function banOrUnbanUser(
+        address _userAddress,
+        uint256 _banStatus
+    ) external onlyOwner {
+        bannedUsers[_userAddress] = _banStatus;
+    }
+
+    /**
+     * @notice Checks if one of the addresses is banned by the admin
+     * @dev Hook that is called before any transfer of tokens. This includes
+     * minting and burning.
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        require(
+            bannedUsers[from] != 1,
+            "The address you are trying to send from is banned"
+        );
+        require(
+            bannedUsers[to] != 1,
+            "The address you are trying to send to is banned"
+        );
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    /**
+     * @notice Only admin can ban/unban users from using the contract
+     * @dev If you want to ban a User pass in the number 1 if you want to unban the user
+     * @dev it is recommended to pass in a number > 1 like 2 since setting
+     * @dev a non-zero to a non-zero value costs only 5000 gas instead of 20_000gas
+     */
+    function showBannedStatus(
+        address _address
+    ) external view returns (uint256) {
+        return bannedUsers[_address];
+    }
+}
--- contracts/MyCappedCoin.sol
+++ contracts/MyCappedCoin.sol
@@ -4,24 +4,19 @@ pragma solidity 0.8.17;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
+/// @title A contract for a basic ERC20 coin which is capped at 100 million token
+/// @author Patrick Zimmerer
+/// @notice This contract is to demo a sample ERC20 capped contract
+/// @dev When deploying you can choose a token name & symbol => deployer == owner
 contract MyCappedCoin is ERC20Capped {
-    uint8 public constant DECIMALS = 18;
     uint256 public constant MAX_SUPPLY = 100_000_000 * 1e18;
     address immutable i_owner;
 
-    constructor(string memory _name, string memory _symbol) {
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) ERC20Capped(MAX_SUPPLY) {
         i_owner = payable(msg.sender);
-    }
-
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+        _mint(msg.sender, 1_000_000 * 10 ** uint256(decimals()));
     }
 }
